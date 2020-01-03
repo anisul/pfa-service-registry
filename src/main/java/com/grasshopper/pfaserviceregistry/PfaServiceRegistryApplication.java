eacb26351582b141e7b1e404ccd379335e438a9d
anisul islam <anis.2803@gmail.com>
2020-01-03 21:13:53 +0100

    Initial Commit

---
+++ src/main/java/com/grasshopper/pfaserviceregistry/PfaServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.grasshopper.pfaserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfaServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfaServiceRegistryApplication.class, args);
+	}
+
+}
